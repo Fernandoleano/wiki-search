348d3c04ff4b33dfb375584cbf56e2580f6a85f4
Fernando Leano <fernandoleano4@gmail.com>
2022-03-24 22:01:52 -0400

    wiki search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa';
+import './style.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState([]);
+  const [information, setInformation] = useState([]);
+
+  // to handle the search results
+  const handleSearch = async event => {
+    // This will prevent refresh from the user
+    event.preventDefault();
+
+    if (search === '') return;
+    const api = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=21&srsearch=${search}`;
+    const response = await fetch(api);
+    
+    // This will throw a error to the user if there is nothing in the search results
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+    
+    setResult(json.query.search);
+    setInformation(json.query.searchinfo);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Fernando Search<FaSearch className="search-icon" /></h1>
+        <form className="search-box" onSubmit={handleSearch}>
+          <input 
+          type="search" 
+          placeholder="What are you looking for?" 
+          value={search}
+          onChange={event => setSearch(event.target.value)}
+          />
+        </form>
+        { (information.totalhits) ? <p>Search Results: {information.totalhits}</p> : ''}
       </header>
+      
+        {result.map((result, i) => {
+          // Get the url
+          const url = `https://en.wikipedia.org/?curid${result.pageid}`;
+
+          return (
+            <div className="result" key={i}>
+              <h3>{result.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
+              <a href={url} target="_blank" rel="nofollow">Read more</a>
+            </div>
+          )
+        })}
     </div>
   );
 }
